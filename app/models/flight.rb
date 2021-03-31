5543433cb5c236d48cdbdd020494f0f58494d89b
ubercj <ubercj@gmail.com>
2021-03-31 17:52:57 -0400

    Improved radio buttons on search form

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -4,6 +4,14 @@ class Flight < ApplicationRecord
   has_many :bookings
   has_many :passengers, through: :bookings
 
+
+  def flight_details
+    "Departing: #{self.origin_port.code}\n
+    Arriving: #{self.destination_port.code}\n
+    Flight date: #{self.formatted_date}\n
+    Flight duration: #{self.duration} minutes"
+  end
+
   def formatted_date
     date.strftime("%m/%d/%Y")
   end
